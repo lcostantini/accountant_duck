d0bfe1229cf3bf58774e8b8d214fd5e986046778
lcostantini <costantinileandro1@gmail.com>
2015-02-04 12:40:58 -0300

    Endpoint for login

--- app.rb
+++ app.rb
@@ -1,22 +1,37 @@
 require 'cuba'
+require 'cuba/render'
 require 'rack/protection'
+require 'slim'
+require 'ohm'
+require 'pry'
 
-Cuba.use Rack::Session::Cookie,
-  secret: "__a_very_long_string__"
+Cuba.use Rack::Session::Cookie, secret: '__a_very_long_string__'
 Cuba.use Rack::Protection
+Cuba.use Rack::Protection::RemoteReferrer
+Cuba.plugin Cuba::Render
+Cuba.settings[:render][:template_engine] = 'slim'
+
+Dir["./models/*.rb"].each  { |rb| require rb  }
+
+def user_logged?
+  res.redirect '/login' unless session[:user]
+end
 
 Cuba.define do
-  on get do
-    unless session[:user]
-      res.redirect '/authentication'
-    end
+  #user_logged?
 
-    on 'authentication' do
-      res.write 'autenticate'
-    end
+  on root do
+    render 'index'
+  end
 
-    on root do
-      res.write 'hola'
+  on 'login' do
+    render 'login', user: User.new
+    on param('user') do |params|
+      user = User.with(:user_name, params['name'])
+      if user && user.password == params['password']
+        session[:user] = user.id
+        res.redirect "/"
+      end
     end
   end
 end
