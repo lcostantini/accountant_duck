c51a0241ae7335a07fcb94009218417d2f72bf7a
lcostantini <costantinileandro1@gmail.com>
2015-02-05 15:55:36 -0300

    Add css.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ Dir["./helpers/*.rb"].each { |rb| require rb }
 Cuba.use Rack::Session::Cookie, secret: '__a_very_long_string__'
 Cuba.use Rack::Protection
 Cuba.use Rack::Protection::RemoteReferrer
+Cuba.use Rack::Static, urls: ['/css'], root: 'public'
 Cuba.plugin Cuba::Render
 Cuba.plugin UserHelpers
 Cuba.settings[:render][:template_engine] = 'slim'
