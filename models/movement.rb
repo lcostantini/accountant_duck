b7aefaea13bd3c86162b4f641a51bcea3fa4de8e
lcostantini <costantinileandro1@gmail.com>
2015-02-08 14:13:07 -0300

    Select for deposit or extraction.

--- models/movement.rb
+++ models/movement.rb
@@ -3,11 +3,14 @@ class Movement < Ohm::Model
   attribute :created_at
   attribute :description
   attribute :price
+  attribute :movement
 
   index :user
   index :created_at
 
   def save
+    self.price = "-#{self.price}" if self.movement == "Extraction"
+    self.movement = self.movement.downcase
     self.created_at = Time.now.strftime("%D") if self.created_at.empty?
     super
   end
