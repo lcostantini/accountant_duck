1d911adb77d07bf82da904f1dd82bfc128064fe9
lcostantini <costantinileandro1@gmail.com>
2015-02-08 20:16:06 -0300

    Config for each environment.

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,6 @@
-require 'cuba'
-require 'cuba/render'
-require 'rack/protection'
-require 'slim'
-require 'ohm'
-require 'pry'
+require "./config/#{ ENV['RACK_ENV'] || 'development' }"
+
+Ohm.redis = Redic.new ENV['REDISTOGO_URL']
 
 Dir["./models/*.rb"].each { |rb| require rb }
 Dir["./helpers/*.rb"].each { |rb| require rb }
-
-Cuba.use Rack::Session::Cookie, secret: '__a_very_long_string__'
-Cuba.use Rack::Protection
-Cuba.use Rack::Protection::RemoteReferrer
-Cuba.use Rack::Static, urls: ['/css'], root: 'public'
-Cuba.plugin Cuba::Render
-Cuba.plugin UserHelpers
-Cuba.settings[:render][:template_engine] = 'slim'
-
