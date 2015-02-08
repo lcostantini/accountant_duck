b420b17a53caf7848739599541483d7f3f787406
lcostantini <costantinileandro1@gmail.com>
2015-02-08 20:22:52 -0300

    Fix config.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,20 @@
 require "./config/#{ ENV['RACK_ENV'] || 'development' }"
 
-Ohm.redis = Redic.new ENV['REDISTOGO_URL']
+require 'cuba'
+require 'cuba/render'
+require 'rack/protection'
+require 'slim'
+require 'ohm'
 
 Dir["./models/*.rb"].each { |rb| require rb }
 Dir["./helpers/*.rb"].each { |rb| require rb }
+
+Ohm.redis = Redic.new ENV['REDISTOGO_URL']
+
+Cuba.use Rack::Session::Cookie, secret: '__a_very_long_string__'
+Cuba.use Rack::Protection
+Cuba.use Rack::Protection::RemoteReferrer
+Cuba.use Rack::Static, urls: ['/css'], root: 'public'
+Cuba.plugin Cuba::Render
+Cuba.plugin UserHelpers
+Cuba.settings[:render][:template_engine] = 'slim'
--- config/development.rb
+++ config/development.rb
@@ -1,18 +1,4 @@
 require 'dotenv'
 Dotenv.load
 
-require 'cuba'
-require 'cuba/render'
-require 'rack/protection'
-require 'slim'
-require 'ohm'
 require 'pry'
-require './app'
-
-Cuba.use Rack::Session::Cookie, secret: '__a_very_long_string__'
-Cuba.use Rack::Protection
-Cuba.use Rack::Protection::RemoteReferrer
-Cuba.use Rack::Static, urls: ['/css'], root: 'public'
-Cuba.plugin Cuba::Render
-Cuba.plugin UserHelpers
-Cuba.settings[:render][:template_engine] = 'slim'
