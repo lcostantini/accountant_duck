3721ac564d99fc5078ed0a1585c0b65f9bf8b152
lcostantini <costantinileandro1@gmail.com>
2015-02-26 15:14:28 -0300

    Test for endpoints session.

--- tests/unit/session_test.rb
+++ tests/unit/session_test.rb
@@ -1,8 +1,24 @@
-#TODO: login and logout user
-#scope do
-#  test 'login user' do
-#    current_user
-#    post '/session/new', { user: { name: 'alagranja', password: 'alagranja' } }
-#    assert User.all.count == 1
-#  end
-#end
+def current_user
+  @user ||= User.with :name, 'alagranja'
+end
+
+scope do
+  test 'login user' do
+    post '/session/new', { user: { name: 'alagranja', password: 'alagranja' } }
+    follow_redirect!
+    assert_equal current_user.id, last_request.env['rack.session'][:user_id]
+  end
+
+  test 'logout user' do
+    get '/session'
+    follow_redirect!
+    assert last_request.env['rack.session'][:user_id].nil?
+  end
+
+  test 'invalid credentials when user login' do
+    post '/session/new', { user: { name: 'alagranja', password: 'bad_password' } }
+    notice = 'Your user or password was incorrect.'
+    assert_equal notice, last_request.env['rack.session'][:notice]
+    assert_equal 302, last_response.status
+  end
+end
