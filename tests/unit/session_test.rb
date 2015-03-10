eab2f81fbcd199f542eea1e4606a5b90b8562ddf
lcostantini <costantinileandro1@gmail.com>
2015-03-10 17:04:08 -0300

    Using session method.

--- tests/unit/session_test.rb
+++ tests/unit/session_test.rb
@@ -6,19 +6,19 @@ scope do
   test 'login user' do
     post '/session/new', { user: { name: 'alagranja', password: 'alagranja' } }
     follow_redirect!
-    assert_equal current_user.id, last_request.env['rack.session'][:user_id]
+    assert_equal current_user.id, last_request.session[:user_id]
   end
 
   test 'logout user' do
     get '/session'
     follow_redirect!
-    assert last_request.env['rack.session'][:user_id].nil?
+    assert last_request.session[:user_id].nil?
   end
 
   test 'invalid credentials when user login' do
     post '/session/new', { user: { name: 'alagranja', password: 'bad_password' } }
     notice = 'Your user or password was incorrect.'
-    assert_equal notice, last_request.env['rack.session'][:notice]
+    assert_equal notice, last_request.session[:notice]
     assert_equal 302, last_response.status
   end
 end
