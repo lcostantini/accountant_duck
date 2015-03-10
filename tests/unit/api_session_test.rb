aea9179367c791217de284d6d39d24c7d5a89b64
lcostantini <costantinileandro1@gmail.com>
2015-03-10 17:05:51 -0300

    Params in body.

--- tests/unit/api_session_test.rb
+++ tests/unit/api_session_test.rb
@@ -8,24 +8,25 @@ end
 
 scope do
   test 'login user' do
+    post 'api/login', { 'user' => { 'name' => 'alagranja', 'password' => 'alagranja' } }.to_json
     @user ||= User.with :name, 'alagranja'
-    post 'api/login', { user: { name: 'alagranja', password: 'alagranja' } }
-    assert_equal @user.id, last_request.env['rack.session'][:user_id]
+    assert_equal @user.id, last_request.session[:user_id]
+    assert_equal 200, last_response.status
   end
 
   test 'logout user' do
     delete 'api/logout'
-    assert last_request.env['rack.session'][:user_id].nil?
+    assert last_request.session[:user_id].nil?
   end
 
   test 'invalid credentials when user login' do
-    post 'api/login', { user: { name: 'alagranja', password: 'bad_password' } }
+    post 'api/login', { 'user' => { 'name' => 'alagranja', 'password' => 'bad_password' } }.to_json
     assert_equal 302, last_response.status
   end
 
   test 'test create a movements' do
-    post 'api/movements', { movement: { price: 300000, description: 'increase', type: 'Deposit' } }
-    assert_equal "{:errors=>\"The user is not logged.\"}", last_response.body
+    post 'api/movements', { 'movement' => { 'price' => '300000', 'description' => 'increase', 'type' => 'Deposit' } }.to_json
+    assert_equal 401, last_response.status
   end
 
   test 'delete a movement' do
