6227540be132867f43377d75a852438099c0c338
lcostantini <costantinileandro1@gmail.com>
2015-03-11 16:00:12 -0300

    Remove the config for the frontend app.

--- config/application.rb
+++ config/application.rb
@@ -10,14 +10,12 @@ require 'rack/cors'
 require 'json'
 
 Dir["./models/*.rb"].each { |rb| require rb }
-Dir["./helpers/*.rb"].each { |rb| require rb }
 
 Ohm.redis = Redic.new(ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379')
 
 Cuba.use Rack::Session::Cookie, secret: '__a_very_long_string__'
 Cuba.use Rack::Protection
 Cuba.use Rack::Protection::RemoteReferrer
-Cuba.use Rack::Static, urls: %w(/css /img /app /node_modules), root: 'public'
 
 Cuba.use Rack::Cors do
   allow do
@@ -27,6 +25,4 @@ Cuba.use Rack::Cors do
 end
 
 Cuba.plugin Cuba::Render
-Cuba.plugin UserHelpers
 Cuba.settings[:render][:template_engine] = 'slim'
-require './lib/accountant_app'
