c5aaa424cc956e480f4be91926a2fc3c7b759f96
lcostantini <costantinileandro1@gmail.com>
2015-09-21 19:06:07 -0300

    Remove old gems unused by the API.

--- config/application.rb
+++ config/application.rb
@@ -1,28 +1,9 @@
 require "./config/#{ ENV['RACK_ENV'] || 'development' }"
-
 require 'cuba'
-require 'cuba/render'
-require 'rack/protection'
-require 'slim'
 require 'ohm'
 require 'digest/sha2'
-require 'rack/cors'
 require 'json'
 
 Dir["./models/*.rb"].each { |rb| require rb }
-
-Ohm.redis = Redic.new(ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379')
-
 Cuba.use Rack::Session::Cookie, secret: '__a_very_long_string__'
-# Cuba.use Rack::Protection
-# Cuba.use Rack::Protection::RemoteReferrer
-
-Cuba.use Rack::Cors do
-  allow do
-    origins '*'
-    resource '*', :headers => :any, :methods => [:get, :post, :put, :delete]
-  end
-end
-
-Cuba.plugin Cuba::Render
-Cuba.settings[:render][:template_engine] = 'slim'
+Ohm.redis = Redic.new(ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379')
