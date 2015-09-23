23b072d3cde4e339418b2af26ff3b9116fca804e
lcostantini <costantinileandro1@gmail.com>
2015-09-23 11:34:11 -0300

    Add config to run the web app.

--- config/application.rb
+++ config/application.rb
@@ -7,3 +7,12 @@ require 'json'
 Dir["./models/*.rb"].each { |rb| require rb }
 Cuba.use Rack::Session::Cookie, secret: '__a_very_long_string__'
 Ohm.redis = Redic.new(ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379')
+
+Cuba.use Rack::Static,
+  :urls => ["/css"],
+  :root => 'public'
+
+Cuba.use Rack::Static,
+  :urls => "/",
+  :root => 'public',
+  :index => 'index.html'
